e62689fc52e962b20aa491cd102ecc4c6d13fd7b
Shubhambhagat3226 <shubhambhagat3220@gmail.com>
2024-12-15 19:32:57 +0530

    can enter username

--- src/main/java/com/example/webSocket_demo/client/App.java
+++ src/main/java/com/example/webSocket_demo/client/App.java
@@ -1,16 +1,22 @@
 package com.example.webSocket_demo.client;
 
 import javax.swing.*;
-import java.util.concurrent.ExecutionException;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                String username = JOptionPane.showInputDialog(null,
+                        "Enter username(max: 16 char): ", "Chat Application", JOptionPane.QUESTION_MESSAGE);
+
+                if (username == null || username.isEmpty() || username.length() > 16) {
+                    JOptionPane.showMessageDialog(null, "Invalid username", "Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
                 ClientGui clientGui = null;
                 try {
-                    clientGui = new ClientGui("Bye-Bye");
+                    clientGui = new ClientGui(username);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
