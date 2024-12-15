29b23c428bc3a93de6e90ef3017bda808391e5fa
Shubhambhagat3226 <shubhambhagat3220@gmail.com>
2024-12-15 18:06:16 +0530

    Frontend is done

--- src/main/java/com/example/webSocket_demo/client/ClientGui.java
+++ src/main/java/com/example/webSocket_demo/client/ClientGui.java
@@ -1,12 +1,16 @@
 package com.example.webSocket_demo.client;
 
+import com.example.webSocket_demo.Message;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class ClientGui extends JFrame {
-    private JPanel connectedUsersPanel;
+    private JPanel connectedUsersPanel, messagePanel;
 
     public ClientGui(String username) {
         super("User: " + username);
@@ -33,10 +37,12 @@ public class ClientGui extends JFrame {
 
     public void addGuiComponent() {
         addConnectedUserComponent();
+        addChartComponent();
     }
 
     public void addConnectedUserComponent() {
         connectedUsersPanel = new JPanel();
+        connectedUsersPanel.setBorder(Utility.addPadding(10,10,10,10));
         connectedUsersPanel.setLayout(new BoxLayout(connectedUsersPanel, BoxLayout.Y_AXIS));
         connectedUsersPanel.setBackground(Utility.SECONDARY_COLOR);
         connectedUsersPanel.setPreferredSize(new Dimension(200, getHeight()));
@@ -47,5 +53,69 @@ public class ClientGui extends JFrame {
         connectedUsersPanel.add(connectedUsersLabel);
 
         add(connectedUsersPanel, BorderLayout.WEST);
+    }
+
+    public void addChartComponent() {
+        JPanel chartPanel = new JPanel();
+        chartPanel.setLayout(new BorderLayout());
+        chartPanel.setBackground(Utility.TRANSPARENT_COLOR);
+
+        messagePanel = new JPanel();
+        messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.Y_AXIS));
+        messagePanel.setBackground(Utility.TRANSPARENT_COLOR);
+        chartPanel.add(messagePanel, BorderLayout.CENTER);
+
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setBorder(Utility.addPadding(10,10,10,10));
+        inputPanel.setLayout(new BorderLayout());
+        inputPanel.setBackground(Utility.TRANSPARENT_COLOR);
+
+        JTextField inputField = new JTextField();
+        inputField.setBackground(Utility.SECONDARY_COLOR);
+        inputField.setForeground(Utility.TEXT_COLOR);
+        inputField.setFont(new Font("Inter", Font.PLAIN, 16));
+        inputField.setPreferredSize(new Dimension(inputPanel.getWidth(), 50));
+        inputField.setBorder(Utility.addPadding(10, 10, 10, 10));
+        inputField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+                    String input = inputField.getText();
+
+                    // EDGE CASE: STOP TO UPLOAD EMPTY MESSAGE
+                    if (input.isEmpty()) return;
+                    inputField.setText("");
+
+                    messagePanel.add(createChatMessageComponent(new Message("Bye-Bye", input)));
+                    repaint();
+                    revalidate();
+                }
+            }
+        });
+        inputPanel.add(inputField, BorderLayout.CENTER);
+
+        chartPanel.add(inputPanel, BorderLayout.SOUTH);
+
+        add(chartPanel, BorderLayout.CENTER);
+    }
+
+    private JPanel createChatMessageComponent(Message message) {
+        JPanel chatMessage = new JPanel();
+        chatMessage.setBackground(Utility.TRANSPARENT_COLOR);
+        chatMessage.setLayout(new BoxLayout(chatMessage, BoxLayout.Y_AXIS));
+        chatMessage.setBorder(Utility.addPadding(20, 20, 20 , 20));
+
+        JLabel usernameLabel = new JLabel(message.getUser());
+        usernameLabel.setFont(new Font("Inter", Font.BOLD, 18));
+        usernameLabel.setForeground(Utility.TEXT_COLOR);
+        chatMessage.add(usernameLabel);
+
+        JLabel messageLabel = new JLabel(message.getMessage());
+        messageLabel.setFont(new Font("Inter", Font.PLAIN, 18));
+        messageLabel.setForeground(Utility.TEXT_COLOR);
+        chatMessage.add(messageLabel);
+
+        return chatMessage;
     }
 }
